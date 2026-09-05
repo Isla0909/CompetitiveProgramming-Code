38b83afb88ee912906ae2c862d449f41519d9541
Isla <1872138949@qq.com>
2026-09-05 14:49:33 +0800

    update

---
+++ Codeforces/Old Problem/Codeforces Round 251 (Div. 2)/d.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <bitset>
+#include <cmath>
+#include <ctime>
+#include <random>
+#include <chrono>
+#include <functional>
+#include <cassert>
+#include <iomanip>
+#include <array>
+#define ff first
+#define se second
+#define endl '\n'
+using namespace std;
+using i32 = signed;
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+using f64 = long double;
+using i128 = __int128;
+using u128 = unsigned __int128;
+constexpr long long inf = 1e18;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+constexpr int N = 2e5 + 10, INF = 0x3f3f3f3f, mod = 1e9 + 7;
+
+void tell(int l, vector<int> &v)
+{
+	for(int i = l; i < v.size(); i ++)
+		cout <<v[i] <<" \n"[i == v.size() - 1];
+}
+
+bool cmp(const pii &a, const pii &b)
+{
+	return a.se < b.se;
+}
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m; cin >>n >>m;
+	vector<int> a(n + 1), b(m + 1);
+	for(int i = 1; i <= n; i ++) cin >>a[i];
+	for(int i = 1; i <= m; i ++) cin >>b[i];
+
+	auto f = [&](int x) -> i64
+	{
+		i64 res = 0;
+		for(int i = 1; i <= n; i ++)
+			if(a[i] < x) res += x - a[i];
+		for(int i = 1; i <= m; i ++)
+			if(b[i] > x) res += b[i] - x;
+		return res;
+	};
+
+	int l = 0, r = 1000000000;
+	while(r - l > 2)
+	{
+		int mid1 = l + (r - l) / 3;
+		int mid2 = r - (r - l) / 3;
+		if(f(mid1) < f(mid2)) r = mid2;
+		else l = mid1;
+	}
+	i64 ans = inf;
+	for(int i = l; i <= r; i ++)
+		ans = min(ans, f(i));
+	cout <<ans <<endl;
+	return 0;
+}
---
+++ Codeforces/Old Problem/Codeforces Round 318 (Div. 2)/b.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <bitset>
+#include <cmath>
+#include <ctime>
+#include <random>
+#include <chrono>
+#include <functional>
+#include <cassert>
+#include <iomanip>
+#include <array>
+#define ff first
+#define se second
+#define endl '\n'
+using namespace std;
+using i32 = signed;
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+using f64 = long double;
+using i128 = __int128;
+using u128 = unsigned __int128;
+constexpr long long inf = 1e18;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+constexpr int N = 2e5 + 10, INF = 0x3f3f3f3f, mod = 1e9 + 7;
+
+void tell(int l, vector<int> &v)
+{
+	for(int i = l; i < v.size(); i ++)
+		cout <<v[i] <<" \n"[i == v.size() - 1];
+}
+
+bool cmp(const pii &a, const pii &b)
+{
+	return a.se < b.se;
+}
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m; cin >>n >>m;
+	vector<vector<int>> adj(n + 1);
+	for(int i = 0; i < m; i ++)
+	{
+		int u, v; cin >>u >>v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	vector<int> deg(n + 1);
+	for(int i = 1; i <= n; i ++)
+		deg[i] = adj[i].size();
+
+	int ans = INF;
+	for(int i = 1; i <= n; i ++)
+	{
+		map<int, int> mp;
+		for(auto x : adj[i])
+			mp[x] = 1;
+		for(int j = i + 1; j <= n; j ++)
+		{
+			if(!mp[j]) continue;
+			vector<int> v;
+			for(auto x : adj[j])
+				if(mp[x] && x != i && x != j) v.push_back(x);
+
+			for(auto x : v)
+			{
+//				cout <<i <<" " <<j <<" " <<x <<endl; 
+				// cout <<deg[i] <<" " <<deg[j] <<" " <<deg[x] <<endl;
+				// cout <<deg[i] + deg[j] + deg[x] - 6 <<endl;
+				ans = min(ans, deg[i] + deg[j] + deg[x] - 6);
+			}
+		}
+	}
+	cout <<(ans == INF ? -1 : ans) <<endl;
+	return 0;
+}
